c54a30b2a2d95d2d7cdb1db5515dcf55a8252672
Peter Cunha <petercunha8@gmail.com>
2018-09-08 11:15:32 -0400

    Add downloads folder to gitignore

--- server/index.js
+++ server/index.js
@@ -1,25 +1,25 @@
-const YoutubeMp3Downloader = require("./lib/downloader");
+const YoutubeMp3Downloader = require('./lib/downloader');
 
 //Configure YoutubeMp3Downloader with your settings
 var YD = new YoutubeMp3Downloader({
-	ffmpegPath: "/usr/local/bin/ffmpeg", // Where is the FFmpeg binary located?
-	outputPath: "./downloads", // Where should the downloaded and encoded files be stored?
-	youtubeVideoQuality: "highest", // What video quality should be used?
+	ffmpegPath: '/usr/local/bin/ffmpeg', // Where is the FFmpeg binary located?
+	outputPath: './downloads', // Where should the downloaded and encoded files be stored?
+	youtubeVideoQuality: 'highest', // What video quality should be used?
 	queueParallelism: 2, // How many parallel downloads/encodes should be started?
 	progressTimeout: 2000 // How long should be the interval of the progress reports
 });
 
 //Download video and save as MP3 file
-YD.download("7ZguAEoNpZw");
+YD.download('7ZguAEoNpZw');
 
-YD.on("finished", function(err, data) {
+YD.on('finished', function (err, data) {
 	console.log(JSON.stringify(data));
 });
 
-YD.on("error", function(error) {
+YD.on('error', function (error) {
 	console.log(error);
 });
 
-YD.on("progress", function(progress) {
+YD.on('progress', function (progress) {
 	console.log(JSON.stringify(progress));
 });
